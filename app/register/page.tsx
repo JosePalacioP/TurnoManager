1278ba78d9cf72c99d7878713bdbaf88bad1523f
Jose Luis <josepalacio329201@correo.itm.edu.co>
2025-12-19 10:31:44 -0500

    Implementación del formulario de registro en el cliente

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,15 +1,53 @@
-<form>
-    <label>Nombre:</label>
-    <br></br>
-    <input></input>
-    <br></br>
-    <label>Email:</label>
-    <br></br>
-    <input></input>
-    <br></br>
-    <label>Contraseña:</label>
-    <br></br>
-    <input type="password"></input>
-    <br></br>
-    <button>Registrarse</button>
-</form>
+"use client";
+import { useState } from "react";
+
+export default function RegisterPage() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    return (
+        <form>
+        <label htmlFor="name">Nombre:</label>
+        <input
+            id="name"
+            name="name"
+            type="name"
+            value={formData.name}
+            required
+            onChange={handleChange}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+            id="email"
+            name="email"
+            type="email"
+            value={formData.name}
+            required
+            onChange={handleChange}
+        />
+        <label htmlFor="password">Contraseña:</label>
+        <input
+            id="password"
+            name="password"
+            type="password"
+            value={formData.name}
+            required
+            onChange={handleChange}
+        />
+        <button type="submit">Registrarse</button>
+        </form>
+    );
+}
