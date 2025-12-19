8b791a4283447b3b3f33ded5c7fb913f07e4ac03
Jose Luis <josepalacio329201@correo.itm.edu.co>
2025-12-19 09:40:17 -0500

    Integración del proyecto para avances en Ubuntu 24.04

---
+++ app/register/page.tsx
@@ -0,0 +1,15 @@
+<form>
+    <label>Nombre:</label>
+    <br></br>
+    <input></input>
+    <br></br>
+    <label>Email:</label>
+    <br></br>
+    <input></input>
+    <br></br>
+    <label>Contraseña:</label>
+    <br></br>
+    <input type="password"></input>
+    <br></br>
+    <button>Registrarse</button>
+</form>
